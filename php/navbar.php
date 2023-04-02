6ecf3c4dff91e65b835f3c20d105aacb34e44e8a
Omar Maadarani <omaadarani@outlook.com>
2023-04-02 09:22:31 -0400

    Update navbar.php

--- php/navbar.php
+++ php/navbar.php
@@ -1,6 +1,6 @@
 		<nav class="navbar navbar-expand-md">
 			<!-- Brand -->
-			<a class="navbar-brand" href="/">AutoBid Trader</a>
+			<a class="navbar-brand" href="/NET-3010-Project/">AutoBid Trader</a>
 
 			<!-- Toggler/collapsibe Button -->
 			<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
@@ -11,18 +11,18 @@
 			<div class="collapse navbar-collapse" id="collapsibleNavbar">
 				<ul class="navbar-nav">
 					<li class="nav-item">
-						<a class="nav-link" href="/">Home</a>
+						<a class="nav-link" href="/NET-3010-Project/">Home</a>
 					</li>
 					<li class="nav-item">
-						<a class="nav-link" href="/Pages/buy.php">Buy Car</a>
+						<a class="nav-link" href="/NET-3010-Project/Pages/buy.php">Buy Car</a>
 					</li>
 					<li class="nav-item">
-						<a class="nav-link" href="/Pages/sell.php">Sell Your Car</a>
+						<a class="nav-link" href="/NET-3010-Project/Pages/sell.php">Sell Your Car</a>
 					</li>
 				</ul>
 				<ul class="navbar-nav justify-content-end">
 					<li class="nav-item" id="login-item">
-						<a class="nav-link" href="/Pages/login.php">Login</a>
+						<a class="nav-link" href="/NET-3010-Project/Pages/login.php">Login</a>
 					</li>
 				</ul>
 			</div>
